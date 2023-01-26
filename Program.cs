f4dd5f1ba3164fe5726851038885f80905c81ef4
Ekaterina-muxa <118264415+Ekaterina-muxa@users.noreply.github.com>
2023-01-26 23:59:17 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string[] strings = { "Kate", "kat", "is", "29", "=-))", "0000", "=p", "London is a capital of" }; 
+var result = strings.Where(s => s.Length <= 3).ToArray(); 
+Console.WriteLine(string.Join(", ", result)); 
+Console.ReadLine();
+
